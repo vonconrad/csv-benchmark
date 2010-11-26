ac165b9bdd03bf87db4c1179a42b22498bec6b7a
vonconrad <sebastian@e-channel.com.au>
2010-11-26 11:42:20 +1030

    Use one-line blocks for readability

--- benchmark.rb
+++ benchmark.rb
@@ -13,34 +13,24 @@ file = 'csv/presidents.csv'
 
 Benchmark.bm do |x|
   x.report('csv') do
-    CSV.open(file, 'r', ',') do |row|
-      row
-    end
+    CSV.open(file, 'r', ',') {|row| row}
   end
   
   x.report('fastercsv') do
-    FasterCSV.foreach(file, :col_sep =>',') do |row|
-      row
-    end
+    FasterCSV.foreach(file, :col_sep =>',') {|row| row}
   end
   
   x.report('ccsv') do
-    Ccsv.foreach(file) do |row|
-      row
-    end
+    Ccsv.foreach(file) {|row| row}
   end
   
   x.report('csvscan') do
     open(file) do |io|
-      CSVScan.scan(io) do |row|
-        row
-      end
+      CSVScan.scan(io) {|row| row}
     end
   end
   
   x.report('excelsior') do
-    Excelsior::Reader.rows(File.open(file, 'rb')) do |row|
-      row
-    end
+    Excelsior::Reader.rows(File.open(file, 'r')) {|row| row}
   end
 end

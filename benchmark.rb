47cd5fd06fe879370a50e8d35e87a4339177bc1c
Kieran Pilkington <kieran776@gmail.com>
2011-02-21 22:22:07 +1300

    Parse both CSV files and test LightCSV

--- benchmark.rb
+++ benchmark.rb
@@ -8,29 +8,34 @@ require 'fastercsv'
 require 'ccsv'
 require 'csvscan'
 require 'excelsior'
+require 'lightcsv'
 
-file = 'csv/geoip.csv'
+['csv/presidents.csv', 'csv/geoip.csv'].each do |file|
+  puts "\nTesting #{file}\n\n"
 
-Benchmark.bm do |x|
-  x.report('csv      ') do
-    CSV.open(file, 'r', ',') {|row| row}
-  end
-  
-  x.report('fastercsv') do
-    FasterCSV.foreach(file, :col_sep =>',') {|row| row}
-  end
+  Benchmark.bm do |x|
+    x.report('csv      ') do
+      CSV.open(file, 'r', ',') {|row| row}
+    end
 
-  x.report('excelsior') do
-    csv = File.open(file, 'r')
-    Excelsior::Reader.rows(csv) {|row| row}
-  end
-  
-  x.report('ccsv     ') do
-    Ccsv.foreach(file) {|row| row}
-  end
-  
-  x.report('csvscan  ') do
-    csv = File.open(file, 'r')
-    CSVScan.scan(csv) {|row| row}
+    x.report('fastercsv') do
+      FasterCSV.foreach(file, :col_sep =>',') {|row| row}
+    end
+
+    x.report('lightcsv ') do
+      LightCsv.foreach(file) {|row| row}
+    end
+
+    x.report('excelsior') do
+      Excelsior::Reader.rows(File.open(file, 'r')) {|row| row}
+    end
+
+    x.report('ccsv     ') do
+      Ccsv.foreach(file) {|row| row}
+    end
+
+    x.report('csvscan  ') do
+      CSVScan.scan(File.open(file, 'r')) {|row| row}
+    end
   end
 end
